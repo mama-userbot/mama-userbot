bae022daaeaf93cf6a3c934b19d420399963a388
mama-userbot <77100101+mama-userbot@users.noreply.github.com>
2021-01-07 15:22:59 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Aree On Ho Gaya Bhenchod!! Abb jake .alive likh ke dekho!! Abb Jaao aur sabki maroo.... Join @HellBot_Official for any help..")
+print("Your Bot is sucessfully deploy & ready for work !! now type .alive for checking me !! Abb Jaao aur sabki bajao.... Join @Mama_userbot_Support for any help..")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
